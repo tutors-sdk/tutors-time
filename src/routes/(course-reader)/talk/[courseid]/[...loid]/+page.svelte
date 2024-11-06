cd3d616ac0c93d0ae371d7d5cf9f7ab70144db65
Eamonn de Leastar <edeleastar@wit.ie>
2024-11-06 07:10:33 +0000

    talk

---
+++ src/routes/(course-reader)/talk/[courseid]/[...loid]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+  import Context from "$lib/ui/learning-objects/structure/Context.svelte";
+  import Talk from "$lib/ui/learning-objects/content/Talk.svelte";
+
+  interface Props {
+    data: PageData;
+  }
+  let { data }: Props = $props();
+</script>
+
+<Context lo={data.lo}>
+  <Talk lo={data.lo} />
+</Context>
