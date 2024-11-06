544da22b0b406b394f15523960df80169bdd6880
Eamonn de Leastar <edeleastar@wit.ie>
2024-11-06 06:54:06 +0000

    load top level course presentation

---
+++ src/routes/(course-reader)/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import "../../app.css";
+  import { currentLo } from "$lib/runes";
+  import CourseShell from "$lib/ui/app-shells/CourseShell.svelte";
+  import type { Snippet } from "svelte";
+
+  type Props = { children: Snippet };
+  let { children }: Props = $props();
+</script>
+
+<svelte:head>
+  <title>{currentLo?.value?.title}</title>
+</svelte:head>
+
+<CourseShell>
+  {@render children()}
+</CourseShell>
