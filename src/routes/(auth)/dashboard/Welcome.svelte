408dac1cb40f0ddb665b03b91a9c3b5adf8e6746
Eamonn de Leastar <edeleastar@wit.ie>
2024-11-06 18:44:41 +0000

    dashboard implementation

---
+++ src/routes/(auth)/dashboard/Welcome.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { tutorsConnectService } from "$lib/services/connect.svelte";
+  import Links from "$lib/ui/navigators/Links.svelte";
+</script>
+
+<div class="to-accent-50 dark:to-accent-900 bg-gradient-to-l from-primary-50 dark:from-primary-900">
+  <div class="container mx-auto justify-between px-8 py-20 lg:flex">
+    <div class="">
+      <p class="text-3xl font-bold">
+        Welcome, {tutorsConnectService.tutorsId.value?.name}!
+      </p>
+    </div>
+    <div>
+      <Links />
+    </div>
+  </div>
+</div>
