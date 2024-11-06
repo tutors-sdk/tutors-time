73aa3062ec200d2911098b469e0c2ed6d89b1f4f
Eamonn de Leastar <edeleastar@wit.ie>
2024-11-06 16:25:03 +0000

    session recovery in layouts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
+  import { tutorsConnectService } from "$lib/services/connect.svelte";
   import "../app.css";
   import { computePosition, autoUpdate, flip, shift, offset, arrow } from "@floating-ui/dom";
   import { initializeStores, storePopup } from "@skeletonlabs/skeleton";
-  let { children } = $props();
+  import type { PageData } from "./$types";
+
+  interface Props {
+    data: PageData;
+    children: import("svelte").Snippet;
+  }
+  let { data, children }: Props = $props();
 
   initializeStores();
   storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+  if (data.user) {
+    tutorsConnectService.reconnect(data.user);
+  }
 </script>
 
 {@render children()}
