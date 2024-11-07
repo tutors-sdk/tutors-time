7f6520b9fa89fc9706c01b585454255f0e20e6d2
Eamonn de Leastar <edeleastar@wit.ie>
2024-11-07 07:28:19 +0000

    share ux

--- src/lib/ui/navigators/tutors-connect/ConnectedProfile.svelte
+++ src/lib/ui/navigators/tutors-connect/ConnectedProfile.svelte
@@ -5,11 +5,16 @@
   import { tutorsConnectService } from "$lib/services/connect.svelte";
   import { Avatar } from "@skeletonlabs/skeleton";
   import { presenceService } from "$lib/services/presence.svelte";
+  import { onMount } from "svelte";
 
   const drawerStore = getDrawerStore();
 
   function logout() {
     tutorsConnectService.disconnect("/");
+  }
+
+  function shareStatusChange() {
+    tutorsConnectService.toggleShare();
   }
 
   const onLineDrawerOpen: any = () => {
@@ -21,8 +26,13 @@
 <button use:popup={{ event: "click", target: "avatar" }}>
   <div class="btn btn-sm space-x-1">
     <div class="relative inline-block">
+      {#if presenceService.studentsOnline.value.length && tutorsConnectService.tutorsId.value?.share}
+        <span class="variant-filled-error badge-icon absolute -right-2 -top-2 z-10 text-white">
+          {presenceService.studentsOnline.value.length}
+        </span>
+      {/if}
       <span class="badge-icon absolute -bottom-2 -right-2 z-10 text-white">
-        {#if tutorsConnectService.tutorsId.value?.share}
+        {#if tutorsConnectService.tutorsId.value?.share === "true"}
           <Icon icon="fluent:presence-available-24-filled" color="rgba(var(--color-success-500))" height="20" />
         {:else}
           <Icon icon="fluent:presence-available-24-regular" color="rgba(var(--color-error-500))" height="20" />
@@ -48,7 +58,20 @@
       </a>
     </li>
     <hr />
-    {#if tutorsConnectService.tutorsId.value?.share}
+    <li class="flex">
+      <!-- svelte-ignore a11y_missing_attribute -->
+      <!-- svelte-ignore a11y_click_events_have_key_events -->
+      <!-- svelte-ignore a11y_no_static_element_interactions -->
+      <a onclick={shareStatusChange}>
+        {#if tutorsConnectService.tutorsId.value?.share === "true"}
+          <Icon icon="fluent:presence-available-24-filled" color="rgba(var(--color-success-500))" height="20" />
+        {:else}
+          <Icon icon="fluent:presence-available-24-regular" color="rgba(var(--color-error-500))" height="20" />
+        {/if}
+        <div class="ml-2">Share Presence</div>
+      </a>
+    </li>
+    {#if tutorsConnectService.tutorsId.value?.share === "true"}
       <li>
         <a href="https://live.tutors.dev/{currentCourse.value?.courseId}" target="_blank" rel="noreferrer">
           <Icon icon="fluent:people-list-24-filled" color="rgba(var(--color-primary-500))" height="20" />
