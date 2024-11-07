e1a8d8fef0297b0696a6a2315748653229989160
Eamonn de Leastar <edeleastar@wit.ie>
2024-11-07 08:18:56 +0000

    home page

---
+++ src/routes/(home)/values/ExperienceCard.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { convertMdToHtml } from "$lib/services/models/markdown-utils";
+  interface Props {
+    link?: string;
+    title?: string;
+    content?: string;
+  }
+
+  let { link = "", title = "", content = "" }: Props = $props();
+  let contentHtml = convertMdToHtml(content);
+</script>
+
+<a href={link} target="_blank" rel="noreferrer">
+  <div class="transition-al card mx-auto my-4 w-80 border-y-8 border-primary-500 hover:scale-105 lg:m-4">
+    <div class="card-header">
+      <h3 class="card-title text-2xl font-bold">{title}</h3>
+    </div>
+    <div class="card-body m-4">
+      <p class="card-text">
+        {@html contentHtml}
+      </p>
+    </div>
+  </div>
+</a>
